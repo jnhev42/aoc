de8dbd91f41fe86b5290341495d59c8d32aa970b
jnhev42 <102880392+jnhev42@users.noreply.github.com>
2022-12-03 18:13:19 +0000

    day3part2 + cleanup

--- 22/day3/solution.js
+++ 22/day3/solution.js
@@ -1,4 +1,3 @@
-
 import { readFileSync } from 'fs';
 
 const splitAt = (string, index) => [
@@ -6,21 +5,34 @@ const splitAt = (string, index) => [
     string.substring(index)
 ];
 
+const uniqueChars = string => Array.from(new Set(Array.from(string)));
+
+const common = (arr1, arr2) => Array.from(arr1.filter(elem => arr2.includes(elem)));
+
 const ascii = char => char.charCodeAt(0);
+
+const groupBy = (n, iter) => {
+    let groups = [[]];
+    for (const elem of iter) {
+        if (groups[groups.length - 1].length == n) {
+            groups.push([]);
+        }
+        groups[groups.length - 1].push(elem);
+    }
+    return groups;
+}
 
 const priority = char => char === char.toUpperCase() ? 
     ascii(char) - ascii('A') + 27 :
     ascii(char) - ascii('a') + 1;
 
 const input = readFileSync('input.txt', {encoding: 'utf-8'});
-const output = input.split('\n').map(backpack => {
-    const [comp1, comp2] = splitAt(backpack, Math.floor(backpack.length / 2))
-        .map(str => Array.from(str))
-        .map(chars => new Set(chars))
-        .map(set => Array.from(set));
-
-    const [common] = comp1.filter(elem => comp2.includes(elem));
-    return priority(common);
-}).reduce((acc, n) => acc + n, 0);
+const output = groupBy(3, input.split('\n'))
+    .map(group => {
+        const [elf1, elf2, elf3] = group.map(uniqueChars);
+        const [allThree] = common(common(elf1, elf2), elf3);
+        return priority(allThree);
+    })
+    .reduce((acc, n) => acc + n, 0);
 
 console.log(`priority sum = ${output}`);
